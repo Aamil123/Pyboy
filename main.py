398c25c607def987999de3c4a3872b38ce4179d3
Aamil <90494101+Aamil123@users.noreply.github.com>
2021-10-01 10:46:26 +0530

    Change the prefix of the bot
    And remove the defult helop commont

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import discord
 import os
 from discord.ext import commands
 
-bot = commands.Bot(command_prefix='.')
+bot = commands.Bot(command_prefix='>', help_command=None)
 
 @bot.command()
 async def load(ctx, extension):
