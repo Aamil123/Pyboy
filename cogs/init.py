a6019e459387ca1566202a723153e56ca705e99e
aamil123 <aamilaroopa@gmail.com>
2021-10-01 10:04:37 +0530

    init file modify for bot onrady function

---
+++ cogs/init.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+
+class BotOnReady(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Bot is online now')
+
+
+def setup(bot):
+    bot.add_cog(Example(bot))
+    
+        
+        
