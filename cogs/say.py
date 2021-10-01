930ccc9a8fe80ad47c55167bbbd1bc554b3a221a
aamil123 <aamilaroopa@gmail.com>
2021-10-01 15:10:51 +0530

    Message commont added

--- cogs/say.py
+++ cogs/say.py
@@ -19,8 +19,12 @@ class Say(commands.Cog):
         e.set_footer(text=f"said by -{ctx.author.display_name}.", icon_url=ctx.author.avatar_url)
         await ctx.send(embed = e)
 
-
-
+    @commands.command()
+    async def say(self, ctx, saymsg = None):
+        if saymsg == None:
+            await ctx.send(f"Please type any message to send{ctx.athour.mention}")
+        await ctx.message.delete()
+        await ctx.send(saymsg)
 
 def setup(bot):
     bot.add_cog(Say(bot))
