03cb42c8ceda2a1ea7ace737be2aab7f42a1013f
aamil123 <aamilaroopa@gmail.com>
2021-10-02 13:39:33 +0530

    Avatar commont added

---
+++ cogs/AvatarModify.py
@@ -0,0 +1,15 @@
+import discord
+from discord.ext import commands
+
+
+class AvatarModify(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def avatar(self, ctx):
+        pi= ctx.author.avatar_url 
+        embed = discord.Embed(title="Your avatar",description="Here is your avatar. I think it looks all cool and 'retro'.")
+        embed.set_image(url = pi)
+        embed.set_footer(text=f"Made by {ctx.author.display_name}.", icon_url=pi)
+        await ctx.send(embed=embed)
